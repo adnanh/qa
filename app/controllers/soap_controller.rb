84a4578808ce6978a9a88467da8835c3b38f768f
Adnan Hajdarevic <adnanh@gmail.com>
2014-03-12 23:44:38 +0100

    minor stuff

--- app/controllers/soap_controller.rb
+++ app/controllers/soap_controller.rb
@@ -1,7 +1,21 @@
 class SoapController < ApplicationController
   soap_service namespace: 'urn:WashOut'
 
+  soap_action 'c_priv',
+              :args => {:name => :string, :description => :string },
+              :return => { :privilege => {:id => :integer, :name => :string, :description => :string} }
   def c_priv
+    privilege = UserPrivilege.new(:name => params[:name], :description => params[:description])
+
+    begin
+      privilege.save!
+    rescue ActiveRecord::RecordInvalid
+      render :soap => nil
+      return
+    end
+
+    render :soap => { :privilege => {:id => privilege.id, :name => privilege.name, :description => privilege.description} }
+    return
   end
 
   def r_priv
