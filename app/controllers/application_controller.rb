13c46a9b428777187c5c00c16ce3e51c226e2c99
AdnanHidic <ahidic2@gmail.com>
2014-03-16 14:39:00 +0100

    Localization through session, parameter or default to engrish.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :set_locale
 
   def index
+  end
+
+  def set_locale
+    I18n.locale = session[:locale] || params[:locale] || I18n.default_locale
   end
 end
