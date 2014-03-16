fef243d7e3b13aa8779cae5ad2eff0ed67d78081
Adnan Hajdarevic <adnanh@gmail.com>
2014-03-16 18:12:36 +0100

    moved to helpers :-)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_filter :require_admin, :only => [:admin_test]
   before_filter :set_locale
 
+  include ApplicationHelper
+
   def index
   end
 
@@ -12,36 +14,6 @@ class ApplicationController < ActionController::Base
 
   def admin_test
     render :json => current_user
-  end
-
-  def not_found
-    raise ActionController::RoutingError.new('Not Found')
-  end
-
-  def require_logged_in
-    authenticate_user!
-  end
-
-  def require_admin
-    if user_signed_in?
-      if current_user.user_privilege_id != 2
-        flash[:alert] = "You do not have sufficient privileges to access the requested page."
-        redirect_to root_path
-      end
-    else
-      authenticate_user!
-    end
-  end
-
-  def is_logged_in?
-    return user_signed_in?
-  end
-
-  def is_admin?
-    if user_signed_in?
-      return current_user.user_privilege_id == 2;
-    end
-    return false
   end
 
   def set_locale
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def require_logged_in
+    authenticate_user!
+  end
+
+  def require_admin
+    if user_signed_in?
+      if current_user.user_privilege_id != 2
+        flash[:alert] = "You do not have sufficient privileges to access the requested page."
+        redirect_to root_path
+      end
+    else
+      authenticate_user!
+    end
+  end
+
+  def is_logged_in?
+    return user_signed_in?
+  end
+
+  def is_admin?
+    if user_signed_in?
+      return current_user.user_privilege_id == 2;
+    end
+    return false
+  end
 end
