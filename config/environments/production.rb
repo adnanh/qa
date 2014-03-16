910e896d6a5dcc557fa3523c0b90bc9e684229b0
Adnan Hajdarevic <adnanh@gmail.com>
2014-03-16 05:44:08 +0100

    sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,13 @@ Qa::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'hajdarevic.net:3000' }
 
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            ENV['MAIL_USERNAME'],
-      password:             ENV['MAIL_PASSWORD'],
-      authentication:       'plain',
-      enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@hajdarevic.net'}
 end
