423829cbb2ab7b0c3779e09a319e80e53ac612b0
zdamir <damirz186@gmail.com>
2014-04-06 18:01:46 +0200

    Custom control 2 (bootstrap ui accordion)

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :image, :styles => {
       :thumb => "100x100#",
-      :small  => "150x150>",
+      :small  => "32x32",
       :medium => "200x200" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
