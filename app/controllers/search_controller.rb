8e090fbf47146244586db2b9bc560338a4f400f9
zdamir <damirz186@gmail.com>
2014-05-16 15:17:46 +0200

    Search update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,25 +1,32 @@
 class SearchController < ApplicationController
-  before_filter :require_logged_in, :except => []
+  before_filter :require_logged_in, :except => [:search_questions]
 
   def search_questions
     respond_to do |format|
       format.json{
         puts params
         page = extract_int params, :page
-        subqueries = []
-        if (params.has_key?(:search_condition))
-          params[:search_condition].each do |element|
+        if (!params.has_key?(:search_by))
+          render :json => reply(false, t(:missing_params))
+        else
+          subqueries = []
+        if(!params[:search_by] == "")
+          params[:search_by].each do |element|
             subqueries.append('tags LIKE %'+element+'% or');
           end
-          subqueries.append('title LIKE %'+:search_condition+'%');
+          subqueries.append('title LIKE %'+params[:search_by]+'%');
           subqueries.join(' ');
-        else
-          query = 'select * from questions  where' + subqueries
-          results = self.connection.execute(query);
+        end
+          if(!subqueries.empty?)
+          query = 'select * from questions where' + subqueries
+          else
+            query = 'select * from questions'
+          end
+          results = SearchQueries.search_questions(query);
           per_page = Rails.application.config.PAGE_SIZE
           @questions = results
           @total_such = @questions.count
-          @questions = @questions.offset((page-1)*per_page).limit(per_page)
+    #      @questions = @questions.offset((page-1)*per_page).limit(per_page)
           render :partial => 'question/questions', :layout => false
         end
       }
@@ -30,4 +37,11 @@ class SearchController < ApplicationController
     end
   end
 
+end
+
+class SearchQueries < ActiveRecord::Base
+
+  def self.search_questions(query)
+    self.connection.execute(query);
+  end
 end
