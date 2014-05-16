ad1a44df0cdd20b2cfe3e45df06ebb3dc807f1be
zdamir <damirz186@gmail.com>
2014-05-16 15:21:32 +0200

    Fix for presentation

--- app/assets/javascripts/angular/controllers/feed.js
+++ app/assets/javascripts/angular/controllers/feed.js
@@ -9,8 +9,8 @@ ctrl_module.controller('FeedCtrl', ['$scope', 'i18n', 'Feed', 'AppAlert', 'Error
          $scope.items_per_page = 10;
          $scope.total_questions = 0;
 
-         $scope.get_page = function (page,search_by) {
-         FeedSrv.get_questions(page,search_by)
+         $scope.get_page = function (page) {
+         FeedSrv.get_questions(page)
          .success(
          function (data) {
          if (data.success) {
