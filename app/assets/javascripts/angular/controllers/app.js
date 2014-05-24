dd225a5d5b600aa42baceb4d272c3306a3ffe1f5
AdnanHidic <ahidic2@gmail.com>
2014-05-24 14:31:28 +0200

    Fixed bug with fixed alerts generated by devise ( https://trello.com/c/mGYCrnRD/103-bugfix-successful-login-alert-propagates-through-all-partials ). Login/logout alerts are now properly displayed and can be closed.

--- app/assets/javascripts/angular/controllers/app.js
+++ app/assets/javascripts/angular/controllers/app.js
@@ -5,5 +5,13 @@ ctrl_module.controller('AppCtrl', ['$scope', 'AppAlert',
         $scope.closeAlert = function (alert) {
             AppAlert.closeAlert(alert);
         };
+
+        $scope.inform_of = function(notice){
+            AppAlert.add("info", notice);
+        };
+
+        $scope.alert_of = function(alert){
+            AppAlert.add("danger", alert);
+        }
     }
 ]);
