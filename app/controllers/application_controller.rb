b6cf05acc55536f56dd31b6bfaf75793b589423e
Adnan Hajdarevic <adnanh@gmail.com>
2014-12-03 19:12:56 +0100

    default locale set to en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = session[:locale] || params[:locale] || I18n.default_locale
+    I18n.locale = I18n.default_locale #session[:locale] || params[:locale] || I18n.default_locale
   end
 
   def ajax_test
