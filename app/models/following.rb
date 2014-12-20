2cd4205c56a0c181aafab9affb13ccb0ac0897ba
Adnan Hidic <ahidic2@gmail.com>
2014-12-20 18:30:19 +0100

    Add uniqueness validation to followed tag

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,6 +1,7 @@
 class Following < ActiveRecord::Base
   belongs_to :user
 
+  validates_uniqueness_of :tag
   validates_format_of :tag, :with => /\A\S*\z/
   validates_length_of :tag, :allow_blank => false, :minimum => 1, :maximum => 255
 end
