ffb22f311064db97f3741fc68864047d929db9b4
Adnan Hidic <ahidic2@gmail.com>
2014-12-21 21:06:39 +0100

    Fixed datetime formatting for momentjs

--- config/initializers/datetime_formats.rb
+++ config/initializers/datetime_formats.rb
@@ -2,4 +2,5 @@
 [Time, Date].map do |c|
   c::DATE_FORMATS[:ddmmyyyyhhmmss_format] = lambda { |t| t.strftime('%d.%m.%Y %H:%M:%S') }
   c::DATE_FORMATS[:ddmmyyyyhhmm_format] = lambda { |t| t.strftime('%d.%m.%Y %H:%M')}
+  c::DATE_FORMATS[:momentjs_format] = lambda { |t| t.strftime('%Y-%m-%d %H:%M:%S') }
 end
